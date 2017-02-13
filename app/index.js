85de35625f7ef24710037eb25f3fed6c5750f08f
Scott Jones <scottdj92@gmail.com>
2017-02-13 15:41:01 -0500

    Add Steph Curry and fix babel parsing

--- app/index.js
+++ app/index.js
@@ -2,10 +2,23 @@ import './main.scss';
 import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
+//import players
+import StephCurry from './players/StephCurry/StephCurry';
+
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello World</h1>
+            <div>
+                <h1>Welcome to the Golden State Warriors home page!</h1>
+                <h2>We need:</h2>
+                <ul>
+                    <li>Player info</li>
+                    <li>player stats</li>
+                    <li>Game info</li>
+                    <li>Schedule</li>
+                </ul>
+                <StephCurry/>
+            </div>
         );
     }
 }
---
+++ app/players/StephCurry/StephCurry.js
@@ -0,0 +1,13 @@
+import './StephCurry.scss';
+import React, { PropTypes } from 'react';
+
+export default class StephCurry extends React.Component {
+    render() {
+        return (
+            <div className='player'>
+                <h1>Stephen Curry</h1>
+                <img src='http://placehold.it/300'/>
+            </div>
+        );
+    }
+}

f87f70236cce82198eb63843cf178b2d897cf5fa
Scott Jones <scottdj92@gmail.com>
2017-02-14 07:59:05 -0500

    Updated tab colors and tab switching behavior

--- app/components/parts/Player/Player.js
+++ app/components/parts/Player/Player.js
@@ -8,10 +8,18 @@ export default class Player extends React.Component {
         }
     }
 
-    determineActiveTab(activeTab) {
-        switch (activeTab) {
+    switchTab(e, key) {
+        switch (key) {
             case 0:
-                return (<ul><li className='is-active'></li></ul>);
+                this.setState({activeTab: 0});
+                break;
+            case 1:
+                this.setState({activeTab: 1});
+                break;
+            case 2:
+                this.setState({activeTab: 2});
+                break;
+            default:
                 break;
         }
     }
@@ -29,9 +37,9 @@ export default class Player extends React.Component {
                     <div className='media-content'>
                         <div className='tabs'>
                             <ul>
-                                <li className='is-active'><a href='#'>Season Stats</a></li>
-                                <li><a href='#'>Career Stats</a></li>
-                                <li><a href='#'>Highlights</a></li>
+                                <li className={(this.state.activeTab === 0) ? 'is-active': null}><a onClick={(e) => this.switchTab(e, 0)} href='#'>Season Stats</a></li>
+                                <li className={(this.state.activeTab === 1) ? 'is-active': null}><a href='#' onClick={(e) => this.switchTab(e, 1)}>Career Stats</a></li>
+                                <li className={(this.state.activeTab === 2) ? 'is-active': null}><a href='#' onClick={(e) => this.switchTab(e, 2)}>Highlights</a></li>
                             </ul>
                         </div>
                     </div>

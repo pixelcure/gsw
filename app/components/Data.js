1326ab76984449963bc9bc5907289f83b95fd02a
Scott Jones <scottdj92@gmail.com>
2017-02-16 10:13:07 -0500

    Add Team ranking styling and update Team component

--- app/components/Data.js
+++ app/components/Data.js
@@ -5,6 +5,9 @@ export function getPlayerStats(name) {
     return NBA.stats.playerProfile({PlayerID: player.playerId})
         .then((stats) => {
             return stats;
+        })
+        .catch((err) => {
+            console.warn(err);
         });
 };
 
@@ -12,12 +15,28 @@ export function getWarriorsTeamInfo() {
     return NBA.stats.teamInfoCommon({TeamID: 1610612744})
         .then((stats) => {
             return stats;
+        })
+        .catch((err) => {
+            console.warn(err);
         });
 };
 
 export function getTeamInfo() {
-    return NBA.stats.homepageV2({PlayerOrTeam: 'Warriors'})
+    return NBA.stats.homepageV2({PlayerOrTeam: 'Team', 'StatType': 'points'})
         .then((data) => {
             return data;
         })
-}
+        .catch((err) => {
+            console.warn(err);
+        });
+};
+
+export function getTeamDashboard() {
+    return NBA.stats.teamPlayerDashboard({TeamID: 1610612744})
+        .then((data) => {
+            return data;
+        })
+        .catch((err) => {
+            console.warn(err);
+        });
+};
--- app/components/parts/Team/Team.js
+++ app/components/parts/Team/Team.js
@@ -1,36 +1,94 @@
 import React from 'react';
 
-import { getTeamInfo } from '../../Data';
+import './Team.scss';
+
+import { getWarriorsTeamInfo, getTeamDashboard } from '../../Data';
 
 export default class Team extends React.Component {
     constructor() {
         super();
+        this.state = {
+            data: null
+        }
     }
 
     componentWillMount() {
-        getTeamInfo()
+        getWarriorsTeamInfo()
             .then((data) => {
                 this.setState({data: data});
-                console.log(data);
             })
             .catch((error) => {
                 console.warn(error);
             });
+
+        // getTeamDashboard()
+        //     .then((data) => {
+        //         this.setState({data: data})
+        //         console.log(data);
+        //     })
+        //     .catch((error) => {
+        //         console.warn(error);
+        //     });
     }
+
+    maybeDisplayTeamStats() {
+        if (this.state.data !== null) {
+            let ranks = this.state.data.teamSeasonRanks[0];
+            console.log(ranks);
+            return (
+                <nav className='level'>
+                    <div className='level-item has-text-centered'>
+                        <div>
+                            <p className='heading'>Assists Per Game</p>
+                            <p className='title'>{ranks.astPg} - {this.transformRankings(ranks.astRank)}</p>
+                        </div>
+                    </div>
+                    <div className='level-item has-text-centered'>
+                        <div>
+                            <p className='heading'> Points Per Game</p>
+                            <p className='title'>{ranks.ptsPg} - {this.transformRankings(ranks.ptsRank)}</p>
+                        </div>
+                    </div>
+                    <div className='level-item has-text-centered'>
+                        <div>
+                            <p className='heading'>Opponent Points Per Game</p>
+                            <p className='title'>{ranks.oppPtsPg} - {this.transformRankings(ranks.oppPtsRank)}</p>
+                        </div>
+                    </div>
+                    <div className='level-item has-text-centered'>
+                        <div>
+                            <p className='heading'>Rebounds Per Game</p>
+                            <p className='title'>{ranks.rebPg} - {this.transformRankings(ranks.rebRank)}</p>
+                        </div>
+                    </div>
+                </nav>
+            );
+        }
+    };
+
+    transformRankings(rank) {
+        switch (rank) {
+            case 1:
+                return '1st';
+                break;
+            case 2:
+                return '2nd'
+                break;
+            case 3:
+                return '3rd'
+                break;
+            default:
+                return rank + 'th';
+                break;
+        }
+    };
 
     render() {
         return (
-            <section className='section'>
-                <div className='heading'>
-                    <h1 className='title'>Current Standings</h1>
-                </div>
+            <section className='section teamDashboard'>
+                <h1 className='title'>Current Rankings</h1>
 
-                <div className='tile is-ancestor'>
-                    <div className='tile'>
-                        <h2 className='subtitle'>Points Per Game</h2>
-                        {/* <p>{this.state.data}</p> */}
-                    </div>
-                </div>
+                {this.maybeDisplayTeamStats()}
             </section>
         );
     }
